6b5af673478b1b37a206e21707ffd5c9fec80b20
GabrielCarvalhoM <c.gabrielmaciel@icloud.com>
2023-02-24 21:02:22 -0300

    calculo entre horas realizado, e feito a conversão em valor também, código bem sujo, devo refatoralo nos proximos commits

--- Ocorrencia/View/ExtraScreen.swift
+++ Ocorrencia/View/ExtraScreen.swift
@@ -34,41 +34,26 @@ class ExtraScreen: UIView  {
         self.namePicker.reloadAllComponents()
 
     }
-
+    
     lazy var namePicker: UIPickerView = {
         let picker = UIPickerView()
         picker.translatesAutoresizingMaskIntoConstraints = false
-        
-        
         picker.delegate = self as UIPickerViewDelegate
         picker.dataSource = self as UIPickerViewDataSource
-        
-        
         return picker
     }()
     
     
     lazy var initialDatePicker: UIDatePicker = {
-       
         let datePc = UIDatePicker()
         datePc.translatesAutoresizingMaskIntoConstraints = false
-        
-        let dateFormatter: DateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd/MM/yyyy hh:mm "
+        datePc.frame(forAlignmentRect: CGRectMake(0.0, 0.0, 320.0, 216.0))
         return datePc
     }()
     
     lazy var finalDatePicker: UIDatePicker = {
-       
         let datePc = UIDatePicker()
         datePc.translatesAutoresizingMaskIntoConstraints = false
-        
-        datePc.datePickerMode = .time
-        datePc.locale = .current
-        let dateFormatter: DateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "hh:mm"
-        
-        
         return datePc
     }()
    
@@ -76,67 +61,97 @@ class ExtraScreen: UIView  {
     lazy var nameLabel: UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-
         label.text = "Selecione seu nome"
-        
-        
-        
         return label
-        
     }()
     
     lazy var departmentNameLabel: UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-
         label.text = "Selecione seu setor"
-        
-        
-        
         return label
-        
     }()
     
     lazy var initialDataAndTime: UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-
         label.text = "Selecione a data e hora inicial:"
-        
-        
-        
         return label
-        
     }()
     
     lazy var finalDataAndTime: UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-
         label.text = "  Hora de encerramento da extra:"
-        
-        
-        
         return label
-        
+    }()
+    
+    
+    lazy var haveInterval: UISwitch = {
+        let sw = UISwitch()
+        sw.translatesAutoresizingMaskIntoConstraints = false
+        return sw
+    }()
+    
+    lazy var haveIntervalLabel: UILabel = {
+        let lb = UILabel()
+        lb.translatesAutoresizingMaskIntoConstraints = false
+        lb.text = "Teve intervalo de uma hora ?"
+        return lb
     }()
     
     lazy var saveButton: UIButton = {
         
        let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        
         button.setTitle("  Registrar extra!!  ", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.layer.borderWidth = 0.8
         button.layer.cornerRadius = 1
         button.addTarget(self, action: #selector(pressEnterButton), for: .touchUpInside)
-        
         button.backgroundColor = UIColor(named: "Color")
-        
-        
         return button
     }()
+    
+    @objc func datePickerValueChanged(_ sender: UIDatePicker) -> String{
+    let dateFormatter: DateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "dd/MM/yyyy HH:mm:ss"
+    let selectedDate: String = dateFormatter.string(from: sender.date)
+            return selectedDate
+        }
+    
+    func test() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm:ss"
+        let startDate = dateFormatter.date(from:datePickerValueChanged(initialDatePicker))!
+        let endDate = dateFormatter.date(from: datePickerValueChanged(finalDatePicker))!
+        
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([ .hour, .minute], from: startDate, to: endDate)
+        
+    
+        let hours = components.hour ?? 0
+        let minutes = components.minute ?? 0
+        
+        let fullHour = ("\(hours).\(minutes)")
+        let fullHourInt = Double(fullHour) ?? 0.0
+        
+        let specialTime = hours.subtractingReportingOverflow(1)
+        let specialTimeDouble = ("\(specialTime.partialValue).\(minutes)")
+        let converted = Double(specialTimeDouble) ?? 0.0
+        
+        let hourvalor = 15.50
+        
+        if !haveInterval.isOn {
+            
+            print("A duração total da sua extra foi de: \(hours):\(minutes):00, e o valor total a receber é: R$\(hourvalor * fullHourInt)")
+            
+        } else {
+            
+            print("A duração total da sua extra foi de: \(specialTime.partialValue):\(minutes):00, e o valor total a receber é: R$\(hourvalor * converted)")
+        }
+
+    }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -150,6 +165,8 @@ class ExtraScreen: UIView  {
         self.addSubview(finalDatePicker)
         self.addSubview(segmentedControl)
         self.addSubview(saveButton)
+        self.addSubview(haveInterval)
+        self.addSubview(haveIntervalLabel)
         
         self.backgroundColor = .white
         cts()
@@ -173,10 +190,7 @@ class ExtraScreen: UIView  {
     }
     
     
-    func printTime() {
-        let index = initialDatePicker.date
-        let index2: Date =
-    }
+    
     
     
     
@@ -186,7 +200,7 @@ class ExtraScreen: UIView  {
     
     @objc func pressEnterButton() {
         
-       printTime()
+       test()
          
     }
 
@@ -219,18 +233,17 @@ class ExtraScreen: UIView  {
             self.finalDatePicker.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             self.finalDatePicker.topAnchor.constraint(equalTo: self.finalDataAndTime.bottomAnchor, constant: 20),
             
-            self.saveButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            self.saveButton.topAnchor.constraint(equalTo: self.finalDatePicker.bottomAnchor, constant: 70)
+            self.haveIntervalLabel.topAnchor.constraint(equalTo: self.finalDatePicker.bottomAnchor, constant: 25),
+            self.haveIntervalLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             
+            self.haveInterval.topAnchor.constraint(equalTo: self.haveIntervalLabel.bottomAnchor, constant: 12),
+            self.haveInterval.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            self.saveButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.saveButton.topAnchor.constraint(equalTo: self.haveInterval.bottomAnchor, constant: 50)
             
         ])
-        
-        
-        
     }
-    
-    
-    
 }
 
 
